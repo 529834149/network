64c528feadb143d6919024adc440f6bc8c37aef0
chenbaojin <529834149@qq.com>
2020-07-22 18:03:36 +0800

    no message

---
+++ resources/views/index/_rightcontent.blade.php
@@ -0,0 +1,268 @@
+<div class="h_right left">
+    <div class="h_r h_hd">
+        <h2 class="h_title h_title1">中美科技战美国观察</h2>
+        <div class="h_hd_top">
+            <div class="h_hd_img">
+                <img src="images/hd.png" alt="">
+            </div>
+            <h3>战略觉醒和战略形成——中美科技战复盘小结 </h3>
+            <p>美国发动的科技战刚刚开打不到一个月时间，情势就发生了很多超乎想象的变化。要理解这场战争的艰巨性和复杂性，起码要从最近十多年中美高科技力量此消彼长、地缘政治...</p>
+        </div>
+            <ul>
+            <li><a href="">IEEE事件与中国学界的觉醒和崛起</a></li>
+            <li><a href="">你知道5G牌照对中国的战略意义有多</a></li>
+            <li><a href="">敢于引领全球，中国5G的战略选择</a></li>
+            <li><a href="">美国会给中国断网吗？必须假定会</a></li>
+            <li><a href="">华为做操作系统如何才能出奇制胜？</a></li>
+            <li><a href="">任正非——中国第一个世界级高科技</a></li>
+            <li><a href="">全球高科技的“去A化”浪潮</a></li>
+            <li><a href="">商务部清单制度标志着中国积极防御</a></li>
+            <li><a href="">中美科技战，中美行业协会和组织该</a></li>
+
+        </ul>
+    </div>
+    <div class="h_r h_ad">
+        <img src="images/hd.png" alt="">
+    </div>
+    <div class="h_r h_technology">
+        <h2 class="h_title h_title1">科技新知生活</h2>
+        <ul>
+            <li><a href=""><span>疫情下的各国“高考”关键词 </span><strong>新闻直</strong></a></li>
+            <li><a href=""><span>印度屏蔽中国app后续：互联网以  </span><strong>平邑人</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+            <li><a href=""><span>疫情下的各国“高考”关键词 </span><strong>新闻直</strong></a></li>
+            <li><a href=""><span>印度屏蔽中国app后续：互联网以  </span><strong>平邑人</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+            <li><a href=""><span>疫情下的各国“高考”关键词 </span><strong>新闻直</strong></a></li>
+            <li><a href=""><span>印度屏蔽中国app后续：互联网以  </span><strong>平邑人</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+        </ul>
+        <ul>
+            <li><a href=""><span>疫情下的各国“高考”关键词 </span><strong>新闻直</strong></a></li>
+            <li><a href=""><span>印度屏蔽中国app后续：互联网以  </span><strong>平邑人</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+            <li><a href=""><span>疫情下的各国“高考”关键词 </span><strong>新闻直</strong></a></li>
+            <li><a href=""><span>印度屏蔽中国app后续：互联网以  </span><strong>平邑人</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+            <li><a href=""><span>疫情下的各国“高考”关键词 </span><strong>新闻直</strong></a></li>
+            <li><a href=""><span>印度屏蔽中国app后续：互联网以  </span><strong>平邑人</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+        </ul>
+        <ul>
+            <li><a href=""><span>疫情下的各国“高考”关键词 </span><strong>新闻直</strong></a></li>
+            <li><a href=""><span>印度屏蔽中国app后续：互联网以  </span><strong>平邑人</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+            <li><a href=""><span>疫情下的各国“高考”关键词 </span><strong>新闻直</strong></a></li>
+            <li><a href=""><span>印度屏蔽中国app后续：互联网以  </span><strong>平邑人</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+            <li><a href=""><span>疫情下的各国“高考”关键词 </span><strong>新闻直</strong></a></li>
+            <li><a href=""><span>印度屏蔽中国app后续：互联网以  </span><strong>平邑人</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+        </ul>
+        <ul>
+            <li><a href=""><span>疫情下的各国“高考”关键词 </span><strong>新闻直</strong></a></li>
+            <li><a href=""><span>印度屏蔽中国app后续：互联网以  </span><strong>平邑人</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+            <li><a href=""><span>疫情下的各国“高考”关键词 </span><strong>新闻直</strong></a></li>
+            <li><a href=""><span>印度屏蔽中国app后续：互联网以  </span><strong>平邑人</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+            <li><a href=""><span>疫情下的各国“高考”关键词 </span><strong>新闻直</strong></a></li>
+            <li><a href=""><span>印度屏蔽中国app后续：互联网以  </span><strong>平邑人</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+        </ul>
+        <ul>
+            <li><a href=""><span>疫情下的各国“高考”关键词 </span><strong>新闻直</strong></a></li>
+            <li><a href=""><span>印度屏蔽中国app后续：互联网以  </span><strong>平邑人</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+            <li><a href=""><span>疫情下的各国“高考”关键词 </span><strong>新闻直</strong></a></li>
+            <li><a href=""><span>印度屏蔽中国app后续：互联网以  </span><strong>平邑人</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+            <li><a href=""><span>疫情下的各国“高考”关键词 </span><strong>新闻直</strong></a></li>
+            <li><a href=""><span>印度屏蔽中国app后续：互联网以  </span><strong>平邑人</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+        </ul>
+        <ul>
+            <li><a href=""><span>疫情下的各国“高考”关键词 </span><strong>新闻直</strong></a></li>
+            <li><a href=""><span>印度屏蔽中国app后续：互联网以  </span><strong>平邑人</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+            <li><a href=""><span>疫情下的各国“高考”关键词 </span><strong>新闻直</strong></a></li>
+            <li><a href=""><span>印度屏蔽中国app后续：互联网以  </span><strong>平邑人</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+            <li><a href=""><span>疫情下的各国“高考”关键词 </span><strong>新闻直</strong></a></li>
+            <li><a href=""><span>印度屏蔽中国app后续：互联网以  </span><strong>平邑人</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+        </ul>
+        <ul>
+            <li><a href=""><span>疫情下的各国“高考”关键词 </span><strong>新闻直</strong></a></li>
+            <li><a href=""><span>印度屏蔽中国app后续：互联网以  </span><strong>平邑人</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+            <li><a href=""><span>疫情下的各国“高考”关键词 </span><strong>新闻直</strong></a></li>
+            <li><a href=""><span>印度屏蔽中国app后续：互联网以  </span><strong>平邑人</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+            <li><a href=""><span>疫情下的各国“高考”关键词 </span><strong>新闻直</strong></a></li>
+            <li><a href=""><span>印度屏蔽中国app后续：互联网以  </span><strong>平邑人</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+        </ul>
+        <ul>
+            <li><a href=""><span>疫情下的各国“高考”关键词 </span><strong>新闻直</strong></a></li>
+            <li><a href=""><span>印度屏蔽中国app后续：互联网以  </span><strong>平邑人</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+            <li><a href=""><span>疫情下的各国“高考”关键词 </span><strong>新闻直</strong></a></li>
+            <li><a href=""><span>印度屏蔽中国app后续：互联网以  </span><strong>平邑人</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+            <li><a href=""><span>疫情下的各国“高考”关键词 </span><strong>新闻直</strong></a></li>
+            <li><a href=""><span>印度屏蔽中国app后续：互联网以  </span><strong>平邑人</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+            <li><a href=""><span>新基建时代，5万亿市值腾讯还有大 </span><strong>何玺</strong></a></li>
+        </ul>
+    </div>
+    <div class="h_r h_ad">
+        <img src="images/st.jpg" alt="">
+    </div>
+    <div class="h_r h_ad">
+        <img src="images/wangxingban.jpg" alt="">
+    </div>
+    <div class="h_r h_ad">
+        <img src="images/jubao.jpg" alt="">
+    </div>
+    <div class="h_r h_ohi">
+        <h2 class="h_title h_title1">OHI名录</h2>
+        <ul>
+            <li><a href=""><div class="h_avatar"><img src="images/kc.png" alt=""></div><span>丁道师</span></a></li>
+            <li><a href=""><div class="h_avatar"><img src="images/kc.png" alt=""></div><span>倪光南</span></a></li>
+            <li><a href=""><div class="h_avatar"><img src="images/kc.png" alt=""></div><span>斯蒂芬·沃尔夫</span></a></li>
+            <li><a href=""><div class="h_avatar"><img src="images/kc.png" alt=""></div><span>丁道师</span></a></li>
+            <li><a href=""><div class="h_avatar"><img src="images/kc.png" alt=""></div><span>克里斯托弗·威尔查金</span></a></li>
+            <li><a href=""><div class="h_avatar"><img src="images/kc.png" alt=""></div><span>丁道师</span></a></li>
+            <li><a href=""><div class="h_avatar"><img src="images/kc.png" alt=""></div><span>丁道师</span></a></li>
+            <li><a href=""><div class="h_avatar"><img src="images/kc.png" alt=""></div><span>丁道师</span></a></li>
+            <li><a href=""><div class="h_avatar"><img src="images/kc.png" alt=""></div><span>丁道师</span></a></li>
+            <li><a href=""><div class="h_avatar"><img src="images/kc.png" alt=""></div><span>丁道师</span></a></li>
+            <li><a href=""><div class="h_avatar"><img src="images/kc.png" alt=""></div><span>丁道师</span></a></li>
+            <li><a href=""><div class="h_avatar"><img src="images/kc.png" alt=""></div><span>丁道师</span></a></li>
+        </ul>
+    </div>
+    <div class="h_r h_ad">
+        <img src="images/st.jpg" alt="">
+    </div>
+    <div class="h_r h_hd">
+        <h2 class="h_title h_title1">网络空间研究</h2>
+        <div class="h_hd_top">
+        <div class="h_hd_img">
+            <img src="images/hd.png" alt="">
+        </div>
+        <h3>战略觉醒和战略形成——中美科技战复盘小结 </h3>
+        <p>美国发动的科技战刚刚开打不到一个月时间，情势就发生了很多超乎想象的变化。要理解这场战争的艰巨性和复杂性，起码要从最近十多年中美高科技力量此消彼长、地缘政治...</p>
+        </div>
+        <ul>
+            <li><a href="">IEEE事件与中国学界的觉醒和崛起</a></li>
+            <li><a href="">你知道5G牌照对中国的战略意义有多</a></li>
+            <li><a href="">敢于引领全球，中国5G的战略选择</a></li>
+            <li><a href="">美国会给中国断网吗？必须假定会</a></li>
+            <li><a href="">华为做操作系统如何才能出奇制胜？</a></li>
+            <li><a href="">任正非——中国第一个世界级高科技</a></li>
+            <li><a href="">全球高科技的“去A化”浪潮</a></li>
+            <li><a href="">商务部清单制度标志着中国积极防御</a></li>
+            <li><a href="">中美科技战，中美行业协会和组织该</a></li>
+
+        </ul>
+    </div>
+    <div class="h_r h_zl">
+        <h2 class="h_title h_title1">专栏作家</h2>
+        <ul>
+            <li><a href=""><div class="h_avatar"><img src="images/kc.png" alt=""></div><span>丁道师</span></a></li>
+            <li><a href=""><div class="h_avatar"><img src="images/kc.png" alt=""></div><span>倪光南</span></a></li>
+            <li><a href=""><div class="h_avatar"><img src="images/kc.png" alt=""></div><span>斯蒂芬·沃尔夫</span></a></li>
+            <li><a href=""><div class="h_avatar"><img src="images/kc.png" alt=""></div><span>丁道师</span></a></li>
+            <li><a href=""><div class="h_avatar"><img src="images/kc.png" alt=""></div><span>克里斯托弗·威尔查金</span></a></li>
+            <li><a href=""><div class="h_avatar"><img src="images/kc.png" alt=""></div><span>丁道师</span></a></li>
+            <li><a href=""><div class="h_avatar"><img src="images/kc.png" alt=""></div><span>丁道师</span></a></li>
+            <li><a href=""><div class="h_avatar"><img src="images/kc.png" alt=""></div><span>丁道师</span></a></li>
+            <li><a href=""><div class="h_avatar"><img src="images/kc.png" alt=""></div><span>丁道师</span></a></li>
+            <li><a href=""><div class="h_avatar"><img src="images/kc.png" alt=""></div><span>丁道师</span></a></li>
+            <li><a href=""><div class="h_avatar"><img src="images/kc.png" alt=""></div><span>丁道师</span></a></li>
+            <li><a href=""><div class="h_avatar"><img src="images/kc.png" alt=""></div><span>丁道师</span></a></li>
+        </ul>
+    </div>
+    <div class="h_r h_hd">
+        <h2 class="h_title h_title1">专题中心</h2>
+        <div class="h_hd_top">
+            <div class="h_hd_img">
+                <img src="images/zt1.jpg" alt="">
+            </div>
+            <h3>战略觉醒和战略形成——中美科技战复盘小结 </h3>
+            <p>美国发动的科技战刚刚开打不到一个月时间，情势就发生了很多超乎想象的变化。要理解这场战争的艰巨性和复杂性，起码要从最近十多年中美高科技力量此消彼长、地缘政治...</p>
+        </div>
+        <div class="h_hd_top">
+            <div class="h_hd_img">
+                <img src="images/zt2.jpg" alt="">
+            </div>
+            <h3>战略觉醒和战略形成——中美科技战复盘小结 </h3>
+            <p>美国发动的科技战刚刚开打不到一个月时间，情势就发生了很多超乎想象的变化。要理解这场战争的艰巨性和复杂性，起码要从最近十多年中美高科技力量此消彼长、地缘政治...</p>
+        </div>
+        <div class="h_hd_top">
+            <div class="h_hd">
+
+            <div class="h_hd_img">
+                <img src="images/zt3.png" alt="">
+            </div>
+            <h3>战略觉醒和战略形成——中美科技战复盘小结 </h3>
+            <p>美国发动的科技战刚刚开打不到一个月时间，情势就发生了很多超乎想象的变化。要理解这场战争的艰巨性和复杂性，起码要从最近十多年中美高科技力量此消彼长、地缘政治...</p>
+        </div>
+        </div>
+    </div>
+    <div class="h_r h_hd">
+        <h2 class="h_title h_title1">活动</h2>
+        <div class="h_hd_top">
+            <div class="h_hd_img">
+                <img src="images/hd.png" alt="">
+            </div>
+            <h3>战略觉醒和战略形成——中美科技战复盘小结 </h3>
+            <p>美国发动的科技战刚刚开打不到一个月时间，情势就发生了很多超乎想象的变化。要理解这场战争的艰巨性和复杂性，起码要从最近十多年中美高科技力量此消彼长、地缘政治...</p>
+        </div>
+        <ul>
+            <li><a href="">IEEE事件与中国学界的觉醒和崛起</a></li>
+            <li><a href="">你知道5G牌照对中国的战略意义有多</a></li>
+            <li><a href="">敢于引领全球，中国5G的战略选择</a></li>
+            <li><a href="">美国会给中国断网吗？必须假定会</a></li>
+            <li><a href="">华为做操作系统如何才能出奇制胜？</a></li>
+            <li><a href="">任正非——中国第一个世界级高科技</a></li>
+            <li><a href="">全球高科技的“去A化”浪潮</a></li>
+            <li><a href="">商务部清单制度标志着中国积极防御</a></li>
+            <li><a href="">中美科技战，中美行业协会和组织该</a></li>
+            <li><a href="">华为做操作系统如何才能出奇制胜</a></li>
+
+        </ul>
+    </div>
+    <div class="h_r h_hd">
+        <h2 class="h_title h_title1">全民诵读</h2>
+        <div class="h_hd_top">
+            <div class="h_hd_img">
+                <img src="images/zt1.jpg" alt="">
+            </div>
+            <h3>战略觉醒和战略形成——中美科技战复盘小结 </h3>
+        </div>
+        <div class="h_hd_top">
+            <div class="h_hd_img">
+                <img src="images/zt2.jpg" alt="">
+            </div>
+            <h3>战略觉醒和战略形成——中美科技战复盘小结 </h3>
+        </div>
+        <div class="h_hd_top">
+            <div class="h_hd">
+
+                <div class="h_hd_img">
+                    <img src="images/zt3.png" alt="">
+                </div>
+                <h3>战略觉醒和战略形成——中美科技战复盘小结 </h3>
+            </div>
+        </div>
+    </div>
+</div>
